d3782f06016a0caa674f36c6c66dd86eb3d770b7
pavel <kalashnikovisme@gmail.com>
2014-08-20 20:43:49 +0400

    some fixes

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module GrantsController
     config.active_support.escape_html_entities_in_json = true
     config.active_record.whitelist_attributes = true
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
     config.assets.version = '1.0'
     config.generators do |g|
       g.fixture_replacement :factory_girl
