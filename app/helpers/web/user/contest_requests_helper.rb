b246f90a91efaac63082dcc45f97087f23e8dc42
pavel <kalashnikovisme@gmail.com>
2014-09-20 05:49:51 +0400

    link viewer

--- app/helpers/web/user/contest_requests_helper.rb
+++ app/helpers/web/user/contest_requests_helper.rb
@@ -1,8 +1,14 @@
 module Web::User::ContestRequestsHelper
-  def current_hint_lead(hints, attribute)
-    if hints
-      current_hint = hints.select { |hint| hint.subject == I18n.t("activerecord.attributes.contest_request.#{attribute}")  }.first
-      current_hint.lead if current_hint
+  def current_hint(attribute)
+    if @hints
+      @hints.select { |hint| hint.subject == I18n.t("activerecord.attributes.contest_request.#{attribute}")  }.first
+    end
+  end
+
+  def popover_for_this_attribute(attribute)
+    hint = current_hint(attribute)
+    if hint
+      "#{hint.lead}<br/>#{link_to(t('.read_more'), hint_path(hint), target: '_blank')}"
     end
   end
 end
